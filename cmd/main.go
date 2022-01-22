26a97f00dff5999cbf43f8a79b9470cafe2b9e42
Anastasia Filatova <56803387+BloodInfection@users.noreply.github.com>
2022-01-22 12:50:08 +0300

    corsa

--- cmd/main.go
+++ cmd/main.go
@@ -70,7 +70,7 @@ func httpResponseModifier(ctx context.Context, w http.ResponseWriter, _ proto.Me
 func cors(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("origin"))
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, ResponseType")
 		h.ServeHTTP(w, r)
