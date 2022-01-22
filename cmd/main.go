1274598558bf9fc72ce9ba0beeb4cefe69c86c4a
Anastasia Filatova <56803387+BloodInfection@users.noreply.github.com>
2022-01-22 13:01:26 +0300

    cors

--- cmd/main.go
+++ cmd/main.go
@@ -73,6 +73,9 @@ func cors(h http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("origin"))
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, ResponseType")
+		if (*r).Method == "OPTIONS" {
+			return
+		}
 		h.ServeHTTP(w, r)
 	})
 }
