96f4c94a267303282ef3fe5606ccae853082d07c
Alexey <54812942+alexeyzer@users.noreply.github.com>
2022-06-09 15:00:31 +0300

    Update login.go

--- internal/user_serivce/login.go
+++ internal/user_serivce/login.go
@@ -7,29 +7,29 @@ import (
 )
 
 func (s *UserApiServiceServer) Login(ctx context.Context, req *desc.LoginRequest) (*desc.LoginResponse, error) {
-	//roles, sessionID, res, err := s.userService.Login(ctx, req)
-	//if err != nil {
-	//	return nil, err
-	//}
+	roles, sessionID, res, err := s.userService.Login(ctx, req)
+	if err != nil {
+		return nil, err
+	}
 	time.Sleep(25 * time.Millisecond)
-	//resp := &desc.LoginResponse{
-	//	Id:                 res.ID,
-	//	Name:               res.Name,
-	//	Surname:            res.Surname,
-	//	Patronymic:         res.Patronymic,
-	//	Phone:              res.Phone,
-	//	Email:              res.Email,
-	//	Session:            sessionID,
-	//	AccessToAdminPanel: len(roles) > 0,
-	//	Roles:              make([]*desc.CreateRoleResponse, 0, len(roles)),
-	//}
-	//for _, role := range roles {
-	//	resp.Roles = append(resp.Roles, &desc.CreateRoleResponse{
-	//		Id:          role.RoleID,
-	//		Name:        role.RoleName,
-	//		Description: "",
-	//	})
-	//}
+	resp := &desc.LoginResponse{
+		Id:                 res.ID,
+		Name:               res.Name,
+		Surname:            res.Surname,
+		Patronymic:         res.Patronymic,
+		Phone:              res.Phone,
+		Email:              res.Email,
+		Session:            sessionID,
+		AccessToAdminPanel: len(roles) > 0,
+		Roles:              make([]*desc.CreateRoleResponse, 0, len(roles)),
+	}
+	for _, role := range roles {
+		resp.Roles = append(resp.Roles, &desc.CreateRoleResponse{
+			Id:          role.RoleID,
+			Name:        role.RoleName,
+			Description: "",
+		})
+	}
 	resp := &desc.LoginResponse{}
 	return resp, nil
 }
